267e0b6ebb2423d6e86e2f16435831b0350c589a
robinPecheurBstorm <robin.pecheur@bstorm.be>
2026-07-30 09:15:23 +0200

    Demo du backtracking

---
+++ dynamic_pogramming/backtracking/demo.py
@@ -0,0 +1,33 @@
+values = [1, 1, 2, 3]
+
+def permutation(tab: list[int]):
+    result = set()
+    path = []
+    used = [False] * len(tab)
+
+    def backtrack():
+        # pour sortir de la récursion
+        if len(path) ==len(tab):
+            result.add(tuple(path))
+            return
+
+        for i in range(len(tab)):
+            # passer les éléments déjà visités
+            if used[i]:
+                continue
+            
+            # faire un choix
+            path.append(tab[i])
+            used[i] = True
+
+            # explorer les autres possibilités de manière recursive
+            backtrack()
+
+            # annuler le choix en cours
+            path.pop()
+            used[i] = False
+
+    backtrack()
+    return result
+
+print(permutation(values))
